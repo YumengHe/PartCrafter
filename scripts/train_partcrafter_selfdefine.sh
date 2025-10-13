9fda44f940e437c74c20381b4c613a7621c7ac4d
Yumeng He <2bidoubi@gmail.com>
2025-10-13 03:11:49 -0700

    update training script

--- scripts/train_partcrafter_selfdefine.sh
+++ scripts/train_partcrafter_selfdefine.sh
@@ -1,26 +1,14 @@
-#!/usr/bin/env bash
+NUM_MACHINES=1
+NUM_LOCAL_GPUS=2
+MACHINE_RANK=0
 
-NUM_MACHINES=${NUM_MACHINES:-1}
-NUM_LOCAL_GPUS=${NUM_LOCAL_GPUS:-2}   # 默认两卡
-MACHINE_RANK=${MACHINE_RANK:-0}
-
-export WANDB_API_KEY="${WANDB_API_KEY:-}"       # 用 wandb 就填，不用可留空
-export CUDA_VISIBLE_DEVICES="${CUDA_VISIBLE_DEVICES:-2,3}"  # 默认用 2,3 两卡
+export WANDB_API_KEY="" # Modify this if you use wandb
 
 accelerate launch \
     --num_machines $NUM_MACHINES \
     --num_processes $(( $NUM_MACHINES * $NUM_LOCAL_GPUS )) \
     --machine_rank $MACHINE_RANK \
-    src/train_partcrafter.py \
-        --config configs/mp8_nt512_selfdefine2.yaml \
-        --output_dir output_partcrafter \
-        --tag result_2gpu \
+    src/train_partcrafter_part.py \
         --pin_memory \
         --allow_tf32 \
-        --gradient_accumulation_steps 8 \
-        --use_ema \
-        --scale_lr \
-        --num_workers 2 \
 $@
-
-# bash scripts/train_partcrafter_selfdefine.sh
